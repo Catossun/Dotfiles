ca2385fc5d27a26c0a6353c163514a620ca65180
Weikeup <boshun2009@gmail.com>
2025-07-28 17:14:20 +0800

    Setup fisher after installed fish

--- setup-scripts/setup-fish.sh
+++ setup-scripts/setup-fish.sh
@@ -9,8 +9,12 @@ sudo apt-add-repository -y ppa:fish-shell/release-4 >> /dev/null
 sudo apt update >> /dev/null 2>&1
 sudo apt install -y fish  >> /dev/null 2>&1
 
-fish --version >> /dev/null && cat << EOF
+# Setup fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish |\
+  fish -c 'source && fisher install jorgebucaran/fisher && fisher update'
+
+cat << EOF
 Install fish success!
-You can now run chsh to change your default shell:
+You can now run "chsh" to change your default shell:
 $ chsh -s /usr/bin/fish
 EOF
