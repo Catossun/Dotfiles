f0355a873944674b844ce02f29e0f4be08844ee3
Webb Chen <boshun2009@gmail.com>
2025-08-08 00:12:56 +0800

    Install required tools

--- setup-scripts/setup-ohmyposh.sh
+++ setup-scripts/setup-ohmyposh.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "Installing oh-my-posh..."
 
+sudo apt update > /dev/null 2>&1
+sudo apt install unzip -y > /dev/null 2>&1
 curl -s https://ohmyposh.dev/install.sh | bash -s
 
 echo "Install success!"
