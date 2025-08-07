2a490dfea37466ac2255142814e766bcd467a9aa
Webb Chen <boshun2009@gmail.com>
2025-08-08 00:12:13 +0800

    Update nvim download URL

--- setup-scripts/setup-nvim.sh
+++ setup-scripts/setup-nvim.sh
@@ -6,10 +6,11 @@ set -e
 echo 'Installing nvim...'
 
 # Download nvim.appimage to /usr/bin/nvim
-sudo curl -sfLo /usr/bin/nvim https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+sudo curl -sfLo /usr/bin/nvim https://github.com/neovim/neovim/releases/download/v0.11.3/nvim-linux-x86_64.appimage
 sudo chmod a+x /usr/bin/nvim
 
 # Install FUSE library to run app image
+sudo apt update >> /dev/null 2>&1
 sudo apt install libfuse2 -y >> /dev/null 2>&1
 
 nvim --version >> /dev/null && echo 'Install success!'
